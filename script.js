b40c0131967a34fb77d684efc4a1a6e3d0f9485b
Jayraje-Shinde <72309001+Jayraje-Shinde@users.noreply.github.com>
2024-08-21 20:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let temp_array = [];
 let main_array = [];
 let operator = "";
+if (main_array[main_array.length - 1] == "") {
+  main_array.pop();
+}
 function operator_clicked(operator_clicked) {
   operator = operator_clicked;
   main_array.push(temp_array.join(""));
   temp_array = [];
-  main_array.push(operator);
-  console.log(main_array);
-  const buttons = document.querySelectorAll('.op-btn');
-  buttons.forEach(button => {
-    button.disabled = true;
-  });
-  document.getElementById('equalto').disabled = false;
+  if (main_array[1] == undefined) {
+    console.log("IF");
+    main_array.push(operator);
+  } else {
+    console.log("else");
+    main_array[1] = operator;
+    console.log(main_array);
+    main_array.pop();
+  }
+
+  const inputfield = document.getElementById('previous-numbers');
+  inputfield.value = main_array.join(" ");
+  // const buttons = document.querySelectorAll('.op-btn');
+  // buttons.forEach(button => {
+  //   button.disabled = true;
+  // });
+  // document.getElementById('equalto').disabled = false;
 }
 function numberclicked(int) {
   let number_pressed = int;
@@ -29,10 +42,17 @@ function clearing() {
   buttons.forEach(button => {
     button.disabled = false;
   });
-  document.getElementById('equalto').disabled = true;
+  // document.getElementById('equalto').disabled = true;
 }
+function plusminus() {
+  let tempnum = temp_array[0];
+  temp_array[0] = -tempnum;
+  const inputfield = document.getElementById('input');
+  inputfield.value = temp_array.join("");
 
+}
 function equalto() {
+  console.log(main_array);
   main_array.push(temp_array.join(""));
   let sum;
   if (main_array[1] == "+") {
@@ -47,14 +67,15 @@ function equalto() {
   if (main_array[1] == "/") {
     sum = Number(main_array[0]) / Number(main_array[2]);
   }
-  if (main_array[1] == "^") {
-    sum = Number(main_array[0]) ** Number(main_array[2]);
-  }
   if (main_array[1] == "%") {
-    sum = Number(main_array[0]) + (Number(main_array[0]) * Number(main_array[2])/100);
+    sum = (Number(main_array[0]) * Number(main_array[2]) * 0.01);
+  }
+  if (main_array == []) {
+    sum = " ";
   }
 
   document.getElementById('input').value = sum;
+  document.getElementById('previous-numbers').value = "";
   temp_array = [];
   main_array = [];
   operator = "";
@@ -62,5 +83,5 @@ function equalto() {
   buttons.forEach(button => {
     button.disabled = false;
   });
-  document.getElementById('equalto').disabled = true;
+  // document.getElementById('equalto').disabled = true;
 }
